7fa59b6f0a317e7a5598a037b5dcaad40a8a20c9
Marie Begue <marie.prandstetter@gmail.com>
2019-11-13 11:12:43 +0100

    work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #READ
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: "task"
+
+  # CREATE
+  get 'task/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 end
